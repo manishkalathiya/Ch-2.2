a63371f21eefc6a6ce56884926b34dc60bb6ce32
HillsKalathiya <100057465+manishkalathiya@users.noreply.github.com>
2024-01-23 19:54:36 +0530

    Add files via upload

---
+++ 2.2.5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define PI 3.14f 
+
+void main() {
+
+  float radius, perimeter, area;
+
+ 
+  printf("Enter radius of the Circle:\n");
+  scanf("%f", & radius);
+
+
+  perimeter = 2 * PI * radius;
+  printf("Perimeter of the circle: %0.4f\n", perimeter);
+
+
+  area = PI * radius * radius;
+  printf("Area of circle: %0.4f\n", area);
+}
